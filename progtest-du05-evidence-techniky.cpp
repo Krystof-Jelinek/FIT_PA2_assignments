316eb65bc8b3d2e5807c70be6a67860c2aa93a78
Krystof Jelinek <krys.jelinek@seznam.cz>
2024-04-17 22:38:51 +0200

    finished homework related to inheritence

--- progtest-du05-evidence-techniky.cpp
+++ progtest-du05-evidence-techniky.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-string create_indentation_string(int indentation, vector<int> & connections){
+string create_indentation_string(int indentation, vector<int> connections){
   string ret;
   for(int i = 0; i < indentation; i++){
     if(find(connections.begin(), connections.end(), i) != connections.end()){
@@ -30,7 +30,7 @@ string create_indentation_string(int indentation, vector<int> & connections){
 
 class CThing{
   public:
-  virtual string printMe(int indentation, vector<int> & connections, bool last_in_list) const = 0;
+  virtual string printMe(int indentation, vector<int> connections, bool last_in_list) const = 0;
 
 };
 
@@ -54,7 +54,7 @@ class CCPU : public CThing{
     frequence = f;
   }
 
-  string printMe(int indentation,vector<int> & connections, bool last_in_list) const override{
+  string printMe(int indentation,vector<int> connections, bool last_in_list) const override{
     string str_ind = create_indentation_string(indentation, connections);
     if(last_in_list){
       return str_ind + "\\-CPU, " + to_string(cores) + " cores @ " + to_string(frequence) + "MHz\n";
@@ -72,7 +72,7 @@ class CMemory : public CThing{
     size = s;
   }
 
-  string printMe(int indentation,vector<int> & connections, bool last_in_list) const override{
+  string printMe(int indentation,vector<int> connections, bool last_in_list) const override{
     string str_ind = create_indentation_string(indentation, connections);
     if(last_in_list){
       return str_ind + "\\-Memory, " + to_string(size) + " MiB\n";
@@ -90,7 +90,7 @@ struct CPartition{
     id = in;
   }
 
-  string printMe(int indentation,vector<int> & connections, bool last_in_list, int number) const{
+  string printMe(int indentation,vector<int> connections, bool last_in_list, int number) const{
     string str_ind = create_indentation_string(indentation, connections);
     if(last_in_list){
       return str_ind + "\\-[" + to_string(number) + "]: " + to_string(size) + " GiB, " + id + "\n";
@@ -119,7 +119,7 @@ class CDisk : public CThing{
     size = s;
   }
 
-  string printMe(int indentation,vector<int> & connections, bool last_in_list) const override{
+  string printMe(int indentation,vector<int> connections, bool last_in_list) const override{
     string ret;
     int counter = 0;
 
@@ -131,16 +131,18 @@ class CDisk : public CThing{
       ret += string_indentation + "+-" + type + ", " + to_string(size) + " GiB\n";
     }
 
+    //TODO tady si nejsem uplne jistej je to nejaky shady
     int loop_counter = 0;
     vector<int> new_connections = connections;
+    indentation += 2;
     new_connections.push_back(indentation);
 
     for(const CPartition & i : partitions){
       if(loop_counter == int(partitions.size() - 1)){
-      ret += i.printMe(indentation + 2,new_connections, true, counter);
+      ret += i.printMe(indentation,new_connections, true, counter);
       }
       else{
-        ret += i.printMe(indentation + 2,new_connections, false, counter);
+        ret += i.printMe(indentation,new_connections, false, counter);
       }
       counter++;
       loop_counter++;
@@ -159,7 +161,7 @@ class CAdress{
   public:
   string adress;
 
-  string printMe(int indentation,vector<int> & connections,bool last_in_list) const{
+  string printMe(int indentation,vector<int> connections,bool last_in_list) const{
     string string_indentation = create_indentation_string(indentation, connections);
     if(last_in_list){
       return string_indentation + "\\-" + adress + "\n";
@@ -181,7 +183,7 @@ class CComputer : public CThing{
     pc_name = in;
   }
 
-  string printMe(int indentation,vector<int> & connections,bool last_in_list) const override{
+  string printMe(int indentation,vector<int> connections,bool last_in_list) const override{
     string res;
     string string_indentation = create_indentation_string(indentation, connections);
     if(last_in_list){
@@ -191,26 +193,47 @@ class CComputer : public CThing{
       res += string_indentation + "+-Host: " + pc_name + "\n";
     }
     
-    for(const CAdress & j : addresses){
-      res += j.printMe(indentation +2, connections, false); //TODO tohle se rozbije pokud bude mit pocitac pouze adresy
+    if(parts.empty()){
+      int cnt = 0;
+      for(const CAdress & j : addresses){
+        if(cnt == int(addresses.size()) - 1){
+          res += j.printMe(indentation + 2, connections, true);
+          continue;
+        }
+        res += j.printMe(indentation +2, connections, false);
+        cnt++;
+      }
     }
+    else{
+      for(const CAdress & j : addresses){
+        res += j.printMe(indentation +2, connections, false); //TODO tohle se snad uz nerozbije pokud bude mit pocitac pouze adresy
+      }
+    }
+
+    
     int counter = 0;
     vector<int> new_connections = connections;
+    indentation += 2;
     new_connections.push_back(indentation);
 
     for(const shared_ptr<CThing> & i : parts){
       if(counter == int(parts.size()) - 1){
-        res += i->printMe(indentation + 2, connections, true);
+        res += i->printMe(indentation, connections, true);
         continue;
       }
-      res += i->printMe(indentation + 2, new_connections, false);
+      res += i->printMe(indentation, new_connections, false);
       counter++;
     }
     return res;
   }
 
   friend ostream & operator << (ostream & ostream, const CComputer & in){
-    ostream << &in;
+    string res;
+    res = in.printMe(-2, {}, false);
+    auto tmp = res.begin() + 2;
+    res.erase(res.begin(), tmp);
+    ostream << res;
+    //ostream << &in;
     return ostream;
   }
 
@@ -248,11 +271,13 @@ class CNetwork : public CThing{
     network_name = in;
   }
 
-  string printMe(int indentation,vector<int> & connections,bool last_in_list) const override{
+  string printMe(int indentation,vector<int> connections,bool last_in_list) const override{
     string ret;
     int counter = 0;
     vector<int> new_connections = connections;
     new_connections.push_back(indentation);
+
+    ret += "Network: " + network_name + "\n";
 
     for(const CComputer & i : computers){
       if(counter == int(computers.size()) - 1){
@@ -298,20 +323,67 @@ std::string toString ( const T_ & x )
 
 int main ()
 {
-  /*
-  CNetwork net("mojesit");
-  CComputer pc("mojepc");
-  pc.addComponent(CDisk(CDisk::MAGNETIC, 20));
-  pc.addComponent(CMemory(20));
+  CComputer r( "progtest.fit.cvut.cz" );
+          r.addAddress ( "147.32.232.142" ) .
+          addComponent ( CCPU ( 8, 2400 ) ) .
+          addComponent ( CCPU ( 8, 1200 ) ) .
+          addComponent ( CMemory ( 2000 ) ).
+          addComponent ( CMemory ( 2000 ) ).
+          addComponent ( CDisk ( CDisk::MAGNETIC, 1500 ) .
+            addPartition ( 50, "/" ) .
+            addPartition ( 5, "/boot" ).
+            addPartition ( 1000, "/var" ) ) .
+          addComponent ( CDisk ( CDisk::SSD, 60 ) .
+            addPartition ( 60, "/data" )  );
 
-  cout << &pc << endl;
+  cout << r;
+  string jap = toString(r);
+  string idk = "Host: progtest.fit.cvut.cz\n"
+    "+-147.32.232.142\n"
+    "+-CPU, 8 cores @ 2400MHz\n"
+    "+-CPU, 8 cores @ 1200MHz\n"
+    "+-Memory, 2000 MiB\n"
+    "+-Memory, 2000 MiB\n"
+    "+-HDD, 1500 GiB\n"
+    "| +-[0]: 50 GiB, /\n"
+    "| +-[1]: 5 GiB, /boot\n"
+    "| \\-[2]: 1000 GiB, /var\n"
+    "\\-SSD, 60 GiB\n"
+    "  \\-[0]: 60 GiB, /data\n";
 
-  net.addComputer(pc);
-  cout << net << endl;
 
-  CDisk disk(CDisk::MAGNETIC, 20);
-  cout << &disk << endl;
-  */
+  assert(toString(r) == 
+    "Host: progtest.fit.cvut.cz\n"
+    "+-147.32.232.142\n"
+    "+-CPU, 8 cores @ 2400MHz\n"
+    "+-CPU, 8 cores @ 1200MHz\n"
+    "+-Memory, 2000 MiB\n"
+    "+-Memory, 2000 MiB\n"
+    "+-HDD, 1500 GiB\n"
+    "| +-[0]: 50 GiB, /\n"
+    "| +-[1]: 5 GiB, /boot\n"
+    "| \\-[2]: 1000 GiB, /var\n"
+    "\\-SSD, 60 GiB\n"
+    "  \\-[0]: 60 GiB, /data\n"
+  );
+
+  CNetwork achjo ( "FIT network" );
+  achjo.addComputer(CComputer ( "imap.fit.cvut.cz" ) .
+          addAddress ( "147.32.232.238" ) .
+          addAddress ( "2001:718:2:2901::238" )).addComputer (
+        CComputer ( "progtest.fit.cvut.cz" ) .
+          addAddress ( "147.32.232.142" ) .
+          addComponent ( CCPU ( 8, 2400 ) ) .
+          addComponent ( CCPU ( 8, 1200 ) ) .
+          addComponent ( CDisk ( CDisk::MAGNETIC, 1500 ) .
+            addPartition ( 50, "/" ) .
+            addPartition ( 5, "/boot" ).
+            addPartition ( 1000, "/var" ) ) .
+          addComponent ( CDisk ( CDisk::SSD, 60 ) .
+            addPartition ( 60, "/data" )  ) .
+          addComponent ( CMemory ( 2000 ) ).
+          addComponent ( CMemory ( 2000 ) ) );
+
 
   CNetwork n ( "FIT network" );
   n . addComputer (
@@ -342,9 +414,6 @@ int main ()
           addAddress ( "2001:718:2:2901::238" ) .
           addComponent ( CMemory ( 8000 ) ) );
   
-  cout << toString ( n );
-
-  /*
   assert ( toString ( n ) ==
     "Network: FIT network\n"
     "+-Host: progtest.fit.cvut.cz\n"
@@ -371,19 +440,22 @@ int main ()
     "  +-2001:718:2:2901::238\n"
     "  +-CPU, 4 cores @ 2500MHz\n"
     "  \\-Memory, 8000 MiB\n" );
-  */
-  /*
+  
+
+  //---------------
   CNetwork x = n;
   auto c = x . findComputer ( "imap.fit.cvut.cz" );
   string coee = toString(*c);
-  cout << toString ( *c );
   assert ( toString ( *c ) ==
     "Host: imap.fit.cvut.cz\n"
     "+-147.32.232.238\n"
     "+-2001:718:2:2901::238\n"
     "+-CPU, 4 cores @ 2500MHz\n"
     "\\-Memory, 8000 MiB\n" );
-
+  /*
+  */
+  //----------
+  
   c -> addComponent ( CDisk ( CDisk::MAGNETIC, 1000 ) .
          addPartition ( 100, "system" ) .
          addPartition ( 200, "WWW" ) .
@@ -445,6 +517,7 @@ int main ()
     "  +-CPU, 4 cores @ 2500MHz\n"
     "  \\-Memory, 8000 MiB\n" );
 
+  /*
   */
 
   return EXIT_SUCCESS;
